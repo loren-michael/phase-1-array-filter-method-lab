3761334418553fd8abdf8c9c3246e2902d278167
loren <lorenmichaelh@gmail.com>
2021-11-30 16:08:15 -0600

    commit tree 640718bce35747077e0f7d7d9890b92ebbea453c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+
+
+//findMatching
+
+// const matching = drivers.filter( drivers => {
+//     return drivers = "bobby"
+// })
+
+// function findMatching(array, name){
+//     let matchingNames = (array.filter(x => x === name));
+//     return matchingNames;
+// }
+
+function findMatching(array, name){
+    return array.filter(x => x.toLowerCase() === name.toLowerCase());
+}
+
+// fuzzyMatch
+
+function fuzzyMatch(array, name){
+    const initial = name.length;
+    return array.filter(x => x.slice(0, initial) === name);
+}
+
+// matchName
+
+function matchName(array, driver){
+    return array.filter(item => item.name === driver);
+}
